cd14da71c00aa1cd78ccff478d03bff3eb50b7cc
Vadim <vadim.marakushin@gmail.com>
2025-05-01 21:29:55 +0300

    hw_3

--- hw-3.js
+++ hw-3.js
@@ -1,34 +1,78 @@
-let password = 'пароль';
+// let password = 'пароль';
 
-let user = {
-    password: ''
-};
-let msg;
+// let user = {
+//     password: ''
+// };
+// let msg;
 
-console.log(user.password = String(prompt("Введите пароль")))
+// console.log(user.password = String(prompt("Введите пароль")))
 
-msg = user.password === password ? alert("Пароль введен верно") : alert("Пароль введен неправильно");
+// msg = user.password === password ? alert("Пароль введен верно") : alert("Пароль введен неправильно");
 
-let c = Number(0);
+// let c = Number(0);
 
-console.log(msg = c > 0 && c < 10 ? alert("Верно") : alert("Неверно"));
-console.log(c = Number(10));
-console.log(msg = c > 0 && c < 10 ? alert("Верно") : alert("Неверно"));
-console.log(c = Number(-3));
-console.log(msg = c > 0 && c < 10 ? alert("Верно") : alert("Неверно"));
-console.log(c = Number(2));
-console.log(msg = c > 0 && c < 10 ? alert("Верно") : alert("Неверно"));
+// console.log(msg = c > 0 && c < 10 ? alert("Верно") : alert("Неверно"));
+// console.log(c = Number(10));
+// console.log(msg = c > 0 && c < 10 ? alert("Верно") : alert("Неверно"));
+// console.log(c = Number(-3));
+// console.log(msg = c > 0 && c < 10 ? alert("Верно") : alert("Неверно"));
+// console.log(c = Number(2));
+// console.log(msg = c > 0 && c < 10 ? alert("Верно") : alert("Неверно"));
 
-let d;
-let e;
+// let d;
+// let e;
 
-console.log(d = Number(132));
-console.log(e = Number(78));
+// console.log(d = Number(132));
+// console.log(e = Number(78));
 
-console.log(msg = d > 100 || e > 100 ? alert("Верно") : alert("Неверно"));
+// console.log(msg = d > 100 || e > 100 ? alert("Верно") : alert("Неверно"));
 
 
-let a = '2';
-let b = '3';
+// let a = '2';
+// let b = '3';
 
-console.log(alert(Number(a) + Number(b)));
+// console.log(alert(Number(a) + Number(b)));
+
+// let monthNumber = Number(12);
+
+// switch (monthNumber) {
+//     case 1:
+//         console.log("Зима")
+//         break;
+//     case 2:
+//         console.log("Зима")
+//         break;
+//     case 12:
+//         console.log("Зима")
+//         break;
+//     case 3:
+//         console.log("Весна")
+//         break;
+//     case 4:
+//         console.log("Весна")
+//         break;
+//     case 5:
+//         console.log("Весна")
+//         break;
+//     case 6:
+//         console.log("Лето")
+//         break;
+//     case 7:
+//         console.log("Лето")
+//         break;
+//     case 8:
+//         console.log("Лето")
+//         break;
+//     case 9:
+//         console.log("Осень")
+//         break;
+//     case 10:
+//         console.log("Осень")
+//         break;
+//     case 11:
+//         console.log("Осень")
+//         break;
+//     default:
+//         console.log("Программа не выполнена введите число от 1 до 12")
+//         break;
+// }
